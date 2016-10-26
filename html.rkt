dabea34595700d2ada51e9b5020f8bd65a6975a0
Jesper Jensen <delusionalLogic@gmail.com>
2016-10-26 10:06:27 +0200

    HTML

---
+++ html.rkt
@@ -0,0 +1,55 @@
+#lang scheme
+(define (list-between sep lst)
+  (match lst
+      ['() (list sep)]
+      [(list x xs ...) (append (list sep x) (list-between sep xs))]
+    )
+  )
+
+(define (list->sep-string sep lst)
+  (apply string-append (list-between sep lst))
+  )
+
+(define (tag name params children)
+  (string-append
+   (format "<~A~A>" name (list->sep-string " " params))
+   (list->sep-string "\n" children)
+   (format "</~A>" name)
+   )
+  )
+
+(define (html . children)
+  (tag "html" '() children)
+  )
+
+(define (body . children)
+  (tag "body" '() children)
+  )
+
+(define (head . children)
+  (tag "head" '() children)
+  )
+
+(define (title . children)
+  (tag "title" '() children)
+  )
+
+(define (h1 params . children)
+  (tag "h1" params children)
+  )
+
+(define div (case-lambda
+               [(params child . children) (tag "div" params (cons child children))]
+               [children (tag "div" '() children)]
+               )
+  )
+
+(define (ul . children)
+  (tag "ul" '() children)
+  )
+
+(define (li . children)
+  (tag "li" '() children)
+  )
+
+(display (html (head (div '("div=\"a\"") "what")) (body (ul (li "as")))))
